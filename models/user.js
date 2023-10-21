5818e1569709efcd5e5b103d5989cbff7ed3556c
Dan <dan.taylor1493@gmail.com>
2023-10-21 11:54:10 +0200

    Define User model is user.js

---
+++ models/user.js
@@ -0,0 +1,26 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = newSchema({
+  email: {
+    type: String,
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+
+// Checks to see if User schema already exists to ensure that database
+// schema is used if it does, before a duplicate is made.
+const User = models.User || model("User", UserSchema);
+
+export default User;
