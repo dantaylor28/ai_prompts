7d07879ac2005df85cac2278bb694d24f391aacc
Dan <dan.taylor1493@gmail.com>
2023-10-26 17:45:44 +0200

    Declare async session function in route.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,24 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//       appDir: true,
+//       serverComponentsExternalPackages: ["mongoose"],
+//     },
+//     images: {
+//       domains: ['lh3.googleusercontent.com'],
+//     },
+//     webpack(config) {
+//       config.experiments = {
+//         ...config.experiments,
+//         topLevelAwait: true,
+//       }
+//       return config
+//     }
+//   }
+  
+//   module.exports = nextConfig
