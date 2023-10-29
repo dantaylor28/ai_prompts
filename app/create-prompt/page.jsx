483bbe9362fb1752f1f93e72c29f9471e4c03476
Dan <dan.taylor1493@gmail.com>
2023-10-29 12:31:33 +0100

    Define fetch api request to send form data

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,13 +7,39 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  const [submitting, setsubmitting] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    // Prevents default browser form submission ie stops page reloading
+    e.preventDefault();
+    // To be used as loader later on
+    setSubmitting(true);
+
+    try {
+      // Try to post form data to api
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      //   If data posts correctly, redirect to home
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
