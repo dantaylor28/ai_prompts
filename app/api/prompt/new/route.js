7f88141c19a67e1c65e6233a670a6e9cb0764549
Dan <dan.taylor1493@gmail.com>
2023-10-29 12:40:27 +0100

    Create new prompt API end point

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,9 @@
+import { connectToDB } from "@utils/database";
+
+export const POST = async (req) => {
+  const { userId, prompt, tag } = await req.JSON();
+
+  try {
+    await connectToDB();
+  } catch (error) {}
+};
