ecda4b77ed2fabb4a20198d5fb3f2094e5eab596
Dan <dan.taylor1493@gmail.com>
2023-10-30 10:07:42 +0100

    Fetch posts and map over them in PromptCardList

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,12 +4,35 @@ import { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {
-    
-  }
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
 
   return (
     <section className="feed">
@@ -23,6 +46,8 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
